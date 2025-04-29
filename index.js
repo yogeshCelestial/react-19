b0e74ade5980276d448f839f0b407b26d24d8efd
Yogesh <yogesh@celestialsys.com>
2025-04-29 20:44:47 +0530

    App Ignited with parcel

---
+++ index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const elem = document.getElementById('root');
+
+const root = ReactDOM.createRoot(elem);
+
+root.render(<App/>);
