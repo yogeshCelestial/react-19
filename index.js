3596077ac90521a945adb59ea9218e21dd12159a
Yogesh <yogesh@celestialsys.com>
2025-05-06 17:17:35 +0530

    react createElement, JSX and react Scripts

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import React from 'react';
 
 const elem = document.getElementById('root');
 
+const heading = React.createElement('h1', { id: 'heading'}, 'Hello React 19');
+
 const root = ReactDOM.createRoot(elem);
 
-root.render(<App/>);
+const jsxHeading = <h1 id='heading'>Hello World!</h1>;
+
+// root.render(heading);
+// heading and jsxHeading are the same, both gives you React elements
+
+root.render(<App />);
